c91d784dc541b60d2bd82f330187227953658608
unknown <roquealejandro18@gmail.com>
2022-10-18 20:50:29 -0400

    NodeJS Templating Agregado

--- Web App/app.js
+++ Web App/app.js
@@ -4,12 +4,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+
 app.use(express.static("public"));
+app.use('css',express.static(__dirname+'public/css'))
+app.use('js',express.static(__dirname+'public/js'))
+app.use('img',express.static(__dirname+'public/img'))
+
+
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine','ejs')
+app.set('views','./views')
 
 
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+'/public/login.html');
+    //res.sendFile(__dirname+'/public/login.html');
+	res.render('index')
 });
 
 
@@ -28,35 +38,53 @@ app.post("/loginPost",(req,res)=>{
 });
 
 app.get("/app",(req,res)=>{
-	res.sendFile(__dirname+'/public/main.html');
+	//res.sendFile(__dirname+'/public/main.html');
+	params = {
+		texto: "",
+		resumen: ""
+	}
+	res.render('main',{params:params})
 });
 
 app.post("/formPost",(req,res)=>{
 	console.log(req.body)
+	if(req.body.tipo == 'resumen'){
+		var options = {
+			'method': 'POST',
+			'url': 'http://127.0.0.1:5000',
+			'headers': {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"text": req.body.text
+				,"lang": req.body.lang
+				,"method":req.body.method
+				,"min":Number(req.body.min)
+				,"max":Number(req.body.max)
+			})
 	
-	var options = {
-		'method': 'POST',
-		'url': 'http://127.0.0.1:5000',
-		'headers': {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-		    "text": req.body.text
-		    ,"lang": req.body.lang
-			,"method":req.body.method
-			,"min":Number(req.body.min)
-			,"max":Number(req.body.max)
-		})
-
-		}; 
-
-	request(options, function (error, response) {
-	  if (error) throw new Error(error);
-	  res.send(response.body);
-	});
+			}; 
+	
+		request(options, function (error, response) {
+		  if (error) {
+			throw new Error(error);
+		  }
+		  else {
+			summary = JSON.parse(response.body)
+			params={
+				texto:req.body.text,
+				resumen: summary.response
+			  }
+			  res.render('main',{params:params});
+		  }
+		});
+	}
+	
 });
 
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is running on port 3000");
 });
+
+
