2192140b2dc11e497576af048545b2f99b6db6e6
unknown <roquealejandro18@gmail.com>
2022-10-31 21:52:43 -0400

    send to mail, missing datbase connection - welcome page name and success page on registration

--- Web App/app.js
+++ Web App/app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
-const upload = require('express-fileupload')
+const upload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const { text } = require("body-parser");
+require("dotenv").config();
 
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+const sendMail = async (msg) => {
+	try{
+		await sgMail.send(msg);
+		console.log("Message sent successfully!");
+	}catch(error){
+		console.log(error);
+		if(error.response){
+			console.error(error.response.body);
+		}
+	}
+};
 
 const app = express();
 
@@ -18,9 +37,24 @@ app.use('img',express.static(__dirname+'public/img'))
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+app.use(cookieParser());
+app.use(session({
+	resave:false,
+	saveUninitialized:false,
+	secret: 'secret-key',
+	credentials: null
+}));
+
 app.set('view engine','ejs')
 app.set('views','./views')
 
+
+let credentials = {
+
+	user_id: 1234,
+	email: 'rrodriguezcabreja@outlook.com'
+}
 
 app.get("/",(req,res)=>{
     //res.sendFile(__dirname+'/public/login.html');
@@ -32,28 +66,45 @@ app.get("/signup",(req,res)=>{
 });
 
 
+app.post("/signup",(req,res)=>{
+	console.log(req.body)
+	res.redirect("/")
+});
+
 app.post("/signin",(req,res)=>{
 
-	const email = req.body.user
-	const password = req.body.pass
+	const user = req.body.user
+	const pass = req.body.pass
 
-	/*
-
+	
 	if(user == "admin" && pass=="admin"){
+		req.session.credentials = credentials;
+		req.session.save();
 		res.redirect("/app");
 	}else{
 		res.redirect("/");
 	}
-*/ 
+ 
 });
+
+app.get("/logout",(req,res)=>{
+	req.session.destroy();
+	res.redirect('/')
+});
+
 
 app.get("/app",(req,res)=>{
 	//res.sendFile(__dirname+'/public/main.html');
-	params = {
-		texto: "",
-		resumen: ""
+	//console.log(req.session);
+	if(req.session.credentials == null ){
+		res.redirect("/")
+	}else {
+		params = {
+			texto: "",
+			resumen: ""
+		}
+		res.render('main',{params:params})
 	}
-	res.render('main',{params:params})
 });
 
 app.post("/formPost",(req,res)=>{
@@ -98,6 +149,15 @@ app.post("/formPost",(req,res)=>{
 			  res.render('main',{params:params});
 		  }
 		});
+	}
+
+	if(req.body.tipo == 'correo'){
+		sendMail({
+			to: req.session.credentials.email,
+			from: "ATSM <atsmpucmm@gmail.com>",
+			subject: "Your Summary Has Arrived!",
+			text: "Your summary:\n"+ req.body.summary
+		})
 	}
 	/*
 	if(req.files){
